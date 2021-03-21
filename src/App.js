74b28654443de8fd74f6493b75c91b6c0ec6f188
dohuyhoangf <dohuyhoangf@gmail.com>
2021-03-22 03:31:32 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,89 @@
 import './App.css';
+const PRODUCTS = [
+  {
+    id: 1,
+    name: 'PRODUCT ITEM NUMBER 1',
+    description: 'Description for product item number 1',
+    image: 'https://via.placeholder.com/200x150',
+    price: '10',
+    quantity: '1',
+  },
+
+  {
+    id: 2,
+    name: 'PRODUCT ITEM NUMBER 2',
+    description: 'Description for product item number 2',
+    image: 'https://via.placeholder.com/200x150',
+    price: '20',
+    quantity: '2',
+  },
+
+  {
+    id: 3,
+    name: 'PRODUCT ITEM NUMBER 3',
+    description: 'Description for product item number 3',
+    image: 'https://via.placeholder.com/200x150',
+    price: '20',
+    quantity: '2',
+  }
+]
+
+const listProducts = PRODUCTS.map((product) =>
+  <li className="row">
+    <div className="col left">
+      <div className="thumbnail">
+        <a href="1">
+          <img src={product.image} alt="" />
+        </a>
+      </div>
+      <div className="detail">
+        <div className="name"><a href="1">{product.name}</a></div>
+        <div className="description">
+          {PRODUCTS.description}
+        </div>
+        <div className="price">{product.price}</div>
+      </div>
+    </div>
+    <div className="col right">
+      <div className="quantity">
+        <input type="number" className="quantity" step={1} defaultValue={product.quantity} />
+      </div>
+      <div className="remove">
+        <svg version="1.1" className="close" xmlns="//www.w3.org/2000/svg" xmlnsXlink="//www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 60 60" enableBackground="new 0 0 60 60" xmlSpace="preserve">
+          <polygon points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812" />
+        </svg>
+      </div>
+    </div>
+  </li>);
+
+// ReactDOM.render(
+//   <ul>{listProducts}</ul>,
+//   document.getElementById("root")
+// );
+
+// function sumItem(){
+//   let sum = product.quantity;
+//   for(let i=0; i<product.id;i++){
+//     sum += sum[i];
+//     return sum;
+//   }
+// }
 
 function App() {
   return (
     <main>
-        <header className="container">
-          <h1>Shopping Cart</h1>
-          <ul className="breadcrumb">
-            <li>Home</li>
-            <li>Shopping Cart</li>
-          </ul>
-          <span className="count">3 items in the bag</span>
-        </header>
-        <section className="container">
-          <ul className="products">
-            <li className="row">
+      <header className="container">
+        <h1>Shopping Cart</h1>
+        <ul className="breadcrumb">
+          <li>Home</li>
+          <li>Shopping Cart</li>
+        </ul>
+        <span className="count"> items in the bag</span>
+      </header>
+      <section className="container">
+        <ul className="products">
+          {listProducts}
+          {/* <li className="row">
               <div className="col left">
                 <div className="thumbnail">
                   <a href="1">
@@ -64,26 +134,26 @@ function App() {
                   </svg>
                 </div>
               </div>
-            </li>
+            </li> */}
+        </ul>
+      </section>
+      <section className="container">
+        <div className="promotion">
+          <label htmlFor="promo-code">Have A Promo Code?</label>
+          <input type="text" id="promo-code" /> <button type="button" />
+        </div>
+        <div className="summary">
+          <ul>
+            <li>Subtotal <span>$21.97</span></li>
+            <li>Tax <span>$5.00</span></li>
+            <li className="total">Total <span>$26.97</span></li>
           </ul>
-        </section>
-        <section className="container">
-          <div className="promotion">
-            <label htmlFor="promo-code">Have A Promo Code?</label>
-            <input type="text" id="promo-code" /> <button type="button" />
-          </div>
-          <div className="summary">
-            <ul>
-              <li>Subtotal <span>$21.97</span></li>
-              <li>Tax <span>$5.00</span></li>
-              <li className="total">Total <span>$26.97</span></li>
-            </ul>
-          </div>
-          <div className="checkout">
-            <button type="button">Check Out</button>
-          </div>
-        </section>
-      </main>
+        </div>
+        <div className="checkout">
+          <button type="button">Check Out</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
